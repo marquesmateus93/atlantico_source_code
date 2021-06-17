3cea320ab3d995d1108ef97a2c6e8d10f04b2454
Marques <mateusmarques1993@gmail.com>
2021-06-17 10:59:53 -0300

    Mudança de layout  Hello World.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,20 @@
-package com.example.demo;
+package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DemoApplication {
+@RestController
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
 	}
 
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
